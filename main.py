d771152faea18110d13acfb868a0ec36d00538f4
sarahyx11 <84320843+sarahyx11@users.noreply.github.com>
2024-08-01 07:26:33 +0000

    game loop

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 # Import statements
+import ...
+
+game = Game()
 
 if __name__ == "__main__":
-    # game.welcome()
-    # game.choose_chicken()
-    # game.day1()
-    # game.day2()
-    # game.day3()
-    # game.day4()
-    # game.day5()
-    # game.ending()
-    print("what a cool game!")
+    game.welcome()
+    game.choose_chicken()
+    for day in range(1, 6):
+        game.set_day(day)
+        game.brief()
+        while not game.day_is_over():
+            choice = game.prompt_player()
+            game.do(choice)
+            game.display_result()
+        game.debrief()
+    game.ending()
+    game.isover()
 
     
     
