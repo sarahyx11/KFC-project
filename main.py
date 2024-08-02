4c24b35f34c6099ce0ac01fbc2252f88f664e815
kaien07 <160471571+kaien07@users.noreply.github.com>
2024-08-02 07:00:35 +0000

    Change method names

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ if __name__ == "__main__":
     game.choose_chicken()
     for day in range(1, 6):
         game.set_day(day)
-        game.brief()
+        game.intro()
         while not game.day_is_over():
             choice = game.prompt_player()
             game.do(choice)
             game.display_result()
         game.debrief()
     game.ending()
-    game.isover()
+    game.is_over()
 
-    
+    colours = {"white": "\x1b[0m", "red": "\x1b[31m", "green": "\x1b[32m", "yellow":  "\x1b[33m", "blue": "\x1b[34m", "purple": "\x1b[35m"}
     
