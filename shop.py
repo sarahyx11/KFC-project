abed4696a37d7ad08b3254990fa7c9e993b8a60e
sarahyx11 <84320843+sarahyx11@users.noreply.github.com>
2024-08-22 16:55:05 +0000

    update shop

--- shop.py
+++ shop.py
@@ -1,28 +1,46 @@
 class Shop:
-    def __init__(self, inventory, price):
+    def __init__(self, inventory, price, item):
         self.inventory = inventory
         self.price = price
+        self.item = item
 
     def get_inventory(self):
         return self.inventory
 
-    def get_item_price(self):
-        print('Check price')
-        selected_item = input('Enter item number:')
-        item_price = price[int(selected_item) - 1]
-        return f' The price is {item_price} price'
-
+    def get_item(self):
+        return self.item
+        
+    def get_price(self):
+        return self.price
+        
+    def get_price_list(self):
+        print()
+        for i in range(len(self.get_inventory())):
+            print(f"{self.get_item()[i]:<15}: ${self.get_price()[i]}")
+        print()
+        
     def purchase_item(self):
-        purchase_number = input('Enter item number:')
-        quantity = input('Enter quantity:')
-        return (int(quantity) * int(purchase_number))
+        print()
+        for i in range(len(self.get_item())):
+            print(f"{i+1}. {self.get_item()[i]}")
+        purchase_number = input('\nEnter item number: ')
+        
+        while not purchase_number.isdigit() or not (1 <= int(purchase_number) <= len(self.get_item())):
+            purchase_number = input('Enter valid item number: ')
+            
+        purchase_number = int(purchase_number)
+        quantity = input('Enter quantity: ')
+        
+        while not quantity.isdigit() or not(1 <= int(quantity) <= int(self.get_inventory()[purchase_number - 1])):
+            print(f"Quantity left: {self.get_inventory()[purchase_number - 1]}")
+            quantity = input('Enter valid quantity: ')
+
+        quantity = int(quantity)
+        self.update_inventory(purchase_number, quantity)
+        
+        return self.get_item()[purchase_number - 1], quantity, self.get_price()[purchase_number - 1] * quantity
 
     def update_inventory(self, purchase_number, quantity):
         self.inventory[int(purchase_number) - 1] -= int(quantity)
 
 
-inventory = [40, 30, 20]  
-price = [2, 3, 4]
-item = {1:'Coke',
-        2: 'Chicken food',
-        3: 'Corn'}
