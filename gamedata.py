627013bac0fee1123568d5492d34ac5726781ad1
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-10-14 11:01:18 +0000

    [feat] add attacks and enemies into gamedata

--- gamedata.py
+++ gamedata.py
@@ -7,4 +7,5 @@ chicken = _gamedata["chicken"]
 inventory = _gamedata["inventory"]
 items = {item["name"]: item for item in _gamedata["items"]}
 shop = _gamedata["shop"]
-
+attacks = _gamedata["attacks"]
+enemies = _gamedata["enemies"]
